ba4dd49394c5257b3711dd01497bfc59d59ac92d
Ilya Paripsa <ilya.paripsa@rea-group.com>
2018-08-27 17:26:00 +1000

    Implement dead or alive rules

--- src/main/scala/gol/DeadOrAlive.scala
+++ src/main/scala/gol/DeadOrAlive.scala
@@ -1,5 +1,13 @@
 package gol
 
 object DeadOrAlive {
-  def isAlive(neighboursCount: Int): Boolean = ???
+  def isAlive(currentlyAlive: Boolean, neighboursCount: Int): Boolean = {
+    neighboursCount match {
+      case x if x < 2 => false
+      case x if (x == 2 && currentlyAlive) || (x == 3) => true
+      case _ => false
+    }
+  }
 }
+
+
--- src/test/scala/gol/DeadOrAliveSpec.scala
+++ src/test/scala/gol/DeadOrAliveSpec.scala
@@ -3,9 +3,25 @@ package gol
 import org.scalatest._
 
 class DeadOrAliveSpec extends FunSpec with Matchers {
-  describe("cell with 0 neighbours") {
-    it("dies") {
-      DeadOrAlive.isAlive(neighboursCount = 3) shouldBe true
+  describe("cell with less than 2 neighbours") {
+    it("dies because of underpopulation") {
+      DeadOrAlive.isAlive(currentlyAlive=true, neighboursCount = 0) shouldBe false
+      DeadOrAlive.isAlive(currentlyAlive=true, neighboursCount = 1) shouldBe false
+    }
+  }
+
+  describe("cell with 2 or 3 neighbours") {
+    it("lives") {
+      DeadOrAlive.isAlive(currentlyAlive=true, neighboursCount = 2) shouldBe true
+      DeadOrAlive.isAlive(currentlyAlive=false, neighboursCount = 2) shouldBe false
+      DeadOrAlive.isAlive(currentlyAlive=true, neighboursCount = 3) shouldBe true
+      DeadOrAlive.isAlive(currentlyAlive=false, neighboursCount = 3) shouldBe true
+    }
+  }
+
+  describe("cell with greater than 3 neighbours") {
+    it("dies by over-crowding") {
+      DeadOrAlive.isAlive(currentlyAlive = true, neighboursCount = 4) shouldBe false
     }
   }
 }
